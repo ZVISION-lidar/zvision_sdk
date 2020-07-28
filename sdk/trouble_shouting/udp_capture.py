e1fea6c187b056c61c3d0d8ca69933f66a47d746
pengfei.cui <gitlabtest@zvision.xyz>
2020-07-28 09:47:45 +0800

    Add python script for udp capture.

---
+++ sdk/trouble_shouting/udp_capture.py
@@ -0,0 +1,56 @@
+#! /bin/python
+
+print "This is a test script"
+
+import socket
+
+
+def udp_pkt_cap(port=None, cnt=125):
+    """Capture udp packet on special port"""
+    if not port:
+        udp_port = 2368
+    else:
+        udp_port = port
+
+    # type: udp socket
+    udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	
+    # Enable the SO_REUSEADDR option
+    udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    new_state = udpSocket.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+    # print("New sock state: %s" % new_state)
+
+    # bind socket
+    try:
+        udpSocket.bind(("", udp_port))
+        udpSocket.settimeout(1000)
+    except socket.error as err_msg:
+        print("Udp socket message:%s" % err_msg)
+		return
+
+    udp_buf = 1304
+    pkt_cnt = cnt
+    messages = []
+    while True:
+        dat, add = udpSocket.recvfrom(udp_buf)
+        if (dat[0:2] == b'\xaa\xaa') or (dat[0:2] == b'\xbb\xbb') or (dat[0:2] == b'\xcc\xcc'):
+            # print(add[0], add[1], hex(ord(dat[2])) + '{:02x}'.format(ord(dat[3])) )
+            # print(add[0], add[1], '0x{:02X} 0x{:02X}'.format(dat[2], dat[3]))
+            messages.append(dat + b'\n')
+            if pkt_cnt > 1:
+                for _ in range(pkt_cnt - 1):
+                    datfrm, add = udpSocket.recvfrom(udp_buf)
+                    messages.append(datfrm + b'\n')
+                break
+            else:
+                break
+
+    return messages  # 返回的是字符串
+	
+
+UDP_PORT = 2368
+print("Start to capture packet on port ", UDP_PORT)
+
+data_cap = udp_pkt_cap(UDP_PORT, 320)
+
+print("Cap packet ok, size is ", len(data_cap))
